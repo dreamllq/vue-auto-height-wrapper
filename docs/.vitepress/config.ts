ae1a9b1dd5bcfb454550f50a1990515084441b80
lvliqi <lvliqi_d@163.com>
2023-04-29 12:48:27 +0800

    doc

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base:'./',
   title: "lc-vue-auto-height-wrapper",
   description: "自动高度组件",
   themeConfig: {
